9e235bb692aaa450cea914f03e7de5d7c5942b56
mikegresham <51673033+mikegresham@users.noreply.github.com>
2021-10-15 07:40:08 +0100

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Michael Gresham on 15/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
